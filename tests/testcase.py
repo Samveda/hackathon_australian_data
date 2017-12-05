cf4c6e820c1f8226691daeb17390aca6f59e57a4
LX <alex19427@gmail.com>
2017-12-05 17:01:48 +0530

    update

--- tests/testcase.py
+++ tests/testcase.py
@@ -8,12 +8,7 @@ class Testing(unittest.TestCase):
 	def setUp(self):
 	    self.path = '../notebook/predicted_class.csv'
 	    self.student_return = pd.read_csv(self.path,header=None)
-<<<<<<< HEAD
 	    self.original_return = pd.read_csv('Original_classes.csv')	
-=======
-	    self.original_return = pd.read_csv('Orginal_classes.csv')
-	    print(self.student_return.dtypes)	
->>>>>>> 7cff6812e70517353d311d1449943709da454de4
 
 	def test_return(self):
             self.assertEqual(self.student_return.shape, (138, 1), "Return value shape does not match expected value")
